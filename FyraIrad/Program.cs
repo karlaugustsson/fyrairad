d5d0500f8478cd1bbef050bcab7465f04801d595
karl <karl.augustsson@gmail.com>
2015-05-06 14:13:46 +0200

    lalalalalalalalalalla

---
+++ FyraIrad/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FyraIrad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WindowHeight = 60;
+            var GameBoard = new GameBoard(new Player("karl", ConsoleColor.Blue), new Player("Erik", ConsoleColor.Red));
+        }
+    }
+}
