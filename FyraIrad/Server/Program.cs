335bedd250dd986545e169ea1eec42a57d24c71a
karl <karl.augustsson@gmail.com>
2015-05-06 11:29:03 +0200

    is suck

---
+++ FyraIrad/Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace Server
+{
+    class Server
+    {
+
+
+        static byte[] Buffer { get; set; }
+        static Socket sct;
+
+        static void Main(string[] args)
+        {
+            sct = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sct.Bind(new IPEndPoint(0, 1234));
+            sct.Listen(100);
+
+            Socket accepted = sct.Accept();
+            Buffer = new byte[accepted.SendBufferSize];
+            int BytesRead = accepted.Receive(Buffer);
+            byte[] formated = new byte[BytesRead];
+            for (int i = 0; i < BytesRead; i++)
+            {
+                formated[i] = Buffer[i];
+            }
+            string strData = Encoding.ASCII.GetString(formated);
+            Console.Write(strData + "\r\n");
+            Console.Read();
+            sct.Close();
+            accepted.Close();
+        }
+    }
+}
